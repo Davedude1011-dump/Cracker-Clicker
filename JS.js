1e03db1b2cf3dc9c56389a6b23a96164239a12a6
davedude1011 <105269128+davedude1011@users.noreply.github.com>
2022-06-16 07:47:31 +0100

    Add files via upload

--- JS.js
+++ JS.js
@@ -1,14 +1,24 @@
-var crackers = 0
+var crackers = 100000000000000
 var crackersPerClick = 1
 
 var isCheese = false
 
 var isUpgrade = false
 var isInfo = false
+var isSkins = false
 
 var upgradeOneAdd = 1
 
 var upgradeOnePrice = 30
+
+var isMeltedCheese = false
+var isMeltedCheeseOn = false
+var isJam = false
+var isJamOn = false
+var isBacon = false
+var isBaconOn = false
+var isGoldenCracker = false
+var isGoldenCrackerOn = false
 
 // Refresh Screen Variables
 
@@ -54,9 +64,52 @@ function  upgradeButtonOneOnclick() {
     refresh()
 }
 
+// skins buying
+
+function skinsButtonOneOnclick() {
+    if (isMeltedCheese === false) {
+       if (crackers >= 100000000) {
+        crackers -= 100000000
+        document.getElementById("skinsOne").style.color = "green"
+        isMeltedCheese = true
+
+        isMeltedCheeseOn = true
+        document.getElementById("meltedCheese").style.display = "block"
+
+        refresh()
+    } 
+    }
+    else {
+        if (isMeltedCheeseOn === false) {
+            isMeltedCheeseOn = true
+            document.getElementById("meltedCheese").style.display = "block"
+            refresh()
+        }
+        else {
+            isMeltedCheeseOn = false
+            document.getElementById("meltedCheese").style.display = "none"
+            refresh()
+        }
+    }
+    
+}
+
 // screen open and close onclick{}
 
 function upgradeToggle() {
+    if (isSkins === true) {
+        document.getElementById("skinsBoxOuter").style.display = "none"
+        isSkins = false
+        if (isUpgrade === false) {
+            document.getElementById("upgradesBoxOuter").style.display = "block"
+            isUpgrade = true
+        }
+        else {
+            document.getElementById("upgradesBoxOuter").style.display = "none"
+            isUpgrade = false
+        }
+    }
+    else {
     if (isUpgrade === false) {
         document.getElementById("upgradesBoxOuter").style.display = "block"
         isUpgrade = true
@@ -65,8 +118,32 @@ function upgradeToggle() {
         document.getElementById("upgradesBoxOuter").style.display = "none"
         isUpgrade = false
     }
+    }
 }
-
+function skinsToggle() {
+if (isUpgrade === true) {
+    document.getElementById("upgradesBoxOuter").style.display = "none"
+    isUpgrade = false
+    if (isSkins === false) {
+        document.getElementById("skinsBoxOuter").style.display = "block"
+        isSkins = true
+    }
+    else {
+        document.getElementById("skinsBoxOuter").style.display = "none"
+        isSkins = false
+    }
+}
+else {
+    if (isSkins === false) {
+        document.getElementById("skinsBoxOuter").style.display = "block"
+        isSkins = true
+    }
+    else {
+        document.getElementById("skinsBoxOuter").style.display = "none"
+        isSkins = false
+    }
+}
+}
 function infoToggle() {
     if (isInfo === false) {
         document.getElementById("infoBoxOuter").style.display = "block"
